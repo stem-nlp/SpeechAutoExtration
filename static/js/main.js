ca093d8520f15a360ccde683307bfaee03715109
zyt <zyt4321@qq.com>
2020-03-10 20:20:42 +0800

    修改饼图样式

--- static/js/main.js
+++ static/js/main.js
@@ -1,28 +1,28 @@
 let data_global = [];
 
-$("#start-button").click(()=>{
+$("#start-button").click(() => {
     let body = $("#input-body").val();
     let popup = $("#popup");
 
-    if (body.length === 0){
+    if (body.length === 0) {
         popup.text("请输入新闻正文").show();
         return
     }
     popup.text("").hide();
-    $("#start-button").attr('disabled','disabled');
+    $("#start-button").attr('disabled', 'disabled');
     $("#button-spin").show();
 
     // http 请求
     $.ajax({
-        url:"/api/model",
+        url: "/api/model",
         type: "post",
         dataType: "json",
-        data: {"body":body},
-        success: (response)=>{
+        data: {"body": body},
+        success: (response) => {
             window.location.hash = "#start-button";
-            if(response.code === 0){
+            if (response.code === 0) {
                 renderOutput(response.data);
-            }else {
+            } else {
                 alert("服务器错误");
             }
             $("#start-button").removeAttr('disabled');
@@ -33,14 +33,14 @@ $("#start-button").click(()=>{
 });
 
 // 渲染结果
-function renderOutput(result){
-    let cnt=0;
+function renderOutput(result) {
+    let cnt = 0;
     $("#result-table tbody").empty();
-    if(result.detail.length === 0){
+    if (result.detail.length === 0) {
         alert("无结果");
         return;
     }
-    for (let line of result.detail){
+    for (let line of result.detail) {
         cnt += 1;
         let tableLine =
             `<tr>
@@ -62,17 +62,17 @@ let btnChart = document.querySelector(".btn-chart");
 let newsList = document.querySelector(".news-list");
 let newCharts = document.querySelector("#news-charts");
 
-btnList.addEventListener("click", ()=>{
+btnList.addEventListener("click", () => {
 
     newsList.style.display = "block";
     btnList.classList.add("active");
-    newCharts.style.display="none";
+    newCharts.style.display = "none";
     btnChart.classList.remove("active");
 });
 
 
 // 图表按钮 -- 点击事件
-btnChart.addEventListener("click", ()=>{
+btnChart.addEventListener("click", () => {
 
     newsList.style.display = "none";     // 列表视图隐藏
     newCharts.style.display = "block";   // 图表视图显示
@@ -93,7 +93,15 @@ function draw_echart(data) {
         },
         tooltip: {
             trigger: 'item',
-            formatter: '{a}'
+            formatter(params) {
+                total_str = "";
+                for(let i=0; i<data.length; i++){
+                    if (data[i].sentiment === params.dataIndex){
+                         total_str = total_str + data[i].content + `<br/>`;
+                    }
+                }
+                return "<div style='font-size: x-small'>" + total_str + "</div>";
+            }
         },
         legend: {
             orient: 'vertical',
@@ -123,17 +131,16 @@ function draw_echart(data) {
     });
 
     // 根据数据渲染饼状图
-    let names = [];
+    let names = ["负面", "正面"];
     let brower = [];
-
+    let positive = 0;
+    let negative = 0;
     // 获取原数据中属性及属性值
-    data.forEach(item=>{
-        names.push(item.speaker);
-        brower.push({
-            name: item.speaker,
-            value: 200
-        });
+    data.forEach(item => {
+        item.sentiment === 0 ? negative++ : positive++
     });
+    brower.push({name: "正面", value: positive});
+    brower.push({name: "负面", value: negative});
 
     // 更改饼状图参数
     myChart.setOption({
@@ -145,21 +152,24 @@ function draw_echart(data) {
         }]
     });
 
-    // 重新获取鼠标滑过事件，浮动框渲染属性值
-    myChart.on("mouseover",function(params){
-        data.map((item,index)=>{
-            if(params.dataIndex==index){
-                params.seriesName = item.content;
-                return params.seriesName;
-            }
-        });
-
-        myChart.setOption({
-            series:[{
-                name: params.seriesName
-            }]
-        });
-    });
+    // // 重新获取鼠标滑过事件，浮动框渲染属性值
+    // myChart.on("mouseover", function (params) {
+    //
+    //     // data.map((item, index) => {
+    //     //     if (params.dataIndex === index) {
+    //     //
+    //     //         // params.seriesName = item.content;
+    //     //         // return params.seriesName;
+    //     //     }
+    //     // });
+    //     params.value = total_str;
+    //     params.seriesName = item.content;
+    //     // myChart.setOption({
+    //     //     series: [{
+    //     //         name: params.seriesName
+    //     //     }]
+    //     // });
+    // });
 
 }
 
